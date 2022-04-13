d79e265816bd79b6b3a252510c542ba61f2fc36e
Pratik Dineshkumar Solanki <pratik.solanki312@gmail.com>
2022-04-13 13:22:55 -0400

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import React , { useState, useEffect } from 'react';
+import React , { useReducer } from 'react';
 import './App.css';
 
-function SecretComponent(){
-  return <h1>Secret information for authorized users only.</h1>
-}
-
-function RegularComponent () {
-   return <h1>Everyone can see this component.</h1>;
-  }
-  
-  /* When the app runs the initial state is going to be happy. */
   function App() {
-    const [emotion, setEmotion] = useState("happy");
-    const [secondary, setSecondary] = useState("tired");
-
-    useEffect(() => {
-       console.log(`It's ${emotion} around here!`);
-      }, [emotion]);
-
-      useEffect(() => {
-        console.log(`It's ${secondary} around here!`);
-       }, [secondary]);
+    const [checked, toggle] =  useReducer(
+      (checked) => !checked,
+      false); 
 
     return (
       <>
-       <h1>Current emotion is {emotion} and {secondary}.</h1>
+       <input type="checkbox" 
+       value={checked} 
+       onChange={toggle} />
 
-       <button onClick={() => setEmotion("happy")}>Make happy </button>
+      <p>{checked ? "checked" : "not checked"}</p>
 
-       <button onClick={() => setSecondary("tired")}> Make tired </button>
-
-       <button onClick={() => setSecondary("crabby")}> Make crabby </button>
-
-       <button onClick={() => setEmotion("frustrated")}> Frustrate </button>
-       
-       <button onClick={() => setEmotion("enthusiastic")}> Enthuse </button>
       </>
     );
   }
