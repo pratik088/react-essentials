3c920052288c79bd217ae457044e60488929f039
Pratik Dineshkumar Solanki <pratik.solanki312@gmail.com>
2022-04-15 01:21:31 -0400

    Fetching with hooks and handling loading states

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React , { useReducer } from 'react';
+import React , { useState , useEffect } from 'react';
 import './App.css';
 
-  function App() {
-    const [checked, toggle] =  useReducer(
-      (checked) => !checked,
-      false); 
+//https://api.github.com/users/pratik088
 
-    return (
-      <>
-       <input type="checkbox" 
-       value={checked} 
-       onChange={toggle} />
+  function App({ login }) {
 
-      <p>{checked ? "checked" : "not checked"}</p>
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-      </>
-    );
+    useEffect(() => {
+      if (!login) return;
+      setLoading(true);
+      fetch(`https://api.github.com/users/pratik088`)
+      .then(response => response.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+    }, [login]);
+
+    if(loading) return <h1>Loading...</h1>;
+    if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+    if(!data) return null;
+
+      return (
+      <div>
+        <h1>{data.login}</h1>
+        <p>{data.location}</p>
+        <img alt={data.login} src={data.avatar_url}/>
+      </div>);
+    
+
+    return <div> No user available.</div>
   }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<App/>
+<App login="pratik088"/>
 );
 
 
